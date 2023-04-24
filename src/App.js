5998c65ada87741e65ecf5596ab056091c898ad2
Jeremy Lehmann <jeremylehmann@Jeremys-MacBook-Pro.local>
2023-04-23 17:31:27 -0700

    Added DataContext

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { useState, useEffect } from 'react';
+
 import Gallery from './components/Gallery';
 import SearchBar from './components/SearchBar';
+import { DataContext } from './context/DataContext';
 
 export default function App() {
   let [search, setSearch] = useState('');
@@ -34,7 +36,9 @@ export default function App() {
     <div>
       <SearchBar handleSearch={handleSearch} />
       {message}
-      <Gallery data={data} />
+      <DataContext.Provider value={data}>
+        <Gallery />
+      </DataContext.Provider>
     </div>
   );
 }
--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,11 +1,16 @@
-import GalleryItem from "./GalleryItem";
+import { useContext } from "react";
 
-export default function Gallery(props) {
-    const display = props.data.map((item, index) => {
+import GalleryItem from "./GalleryItem";
+import { DataContext } from "../context/DataContext";
+
+export default function Gallery() {
+    const data = useContext(DataContext);
+
+    const display = data.map((item, index) => {
         return (
             <GalleryItem item={item} key={index} />
         )
-    })
+    });
 
     return (
         <div>
